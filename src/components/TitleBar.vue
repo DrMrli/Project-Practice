7828047c449531b0d869d80907ff34650d170cc6
libohandeicloud-web <libohandeicloud@icloud.com>
2026-03-04 09:49:59 +0800

    添加前进和后退图标

--- src/components/TitleBar.vue
+++ src/components/TitleBar.vue
@@ -7,10 +7,10 @@
       <!-- 新增导航按钮组 -->
       <div class="nav-buttons">
         <button class="nav-btn" @click="goBack" :disabled="!canGoBack">
-          <svg viewBox="0 0 24 24" width="16" height="16" fill="currentColor"><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg>
+          <img src="../assets/后退.svg" alt="后退" width="16" height="16" style="filter: invert(1);" />
         </button>
         <button class="nav-btn" @click="goForward" :disabled="!canGoForward">
-          <svg viewBox="0 0 24 24" width="16" height="16" fill="currentColor"><path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"/></svg>
+          <img src="../assets/前进.svg" alt="前进" width="16" height="16" style="filter: invert(1);" />
         </button>
         <button class="nav-btn" @click="reloadOrStop" :disabled="isLoading">
           <svg v-if="isLoading" viewBox="0 0 24 24" width="16" height="16" fill="currentColor"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg>
